dea5443995f1992dcecffc7a4e5dcb12313a06c5
Tamer Shlash <mr.tamershlash@gmail.com>
2016-11-11 03:44:59 +0800

    resized to 1280x720

--- screenshot.js
+++ screenshot.js
@@ -1,7 +1,7 @@
 var page = require('webpage').create();
 var fs = require('fs');
 
-page.viewportSize = { width: 3840, height: 2160 };
+page.viewportSize = { width: 1280, height: 720 };
 
 page.open('file:///home/tamer/git/mortalchess/pgn_recorder/player.html', function () {
   setTimeout(function() { // Add a little delay before capturing the image
